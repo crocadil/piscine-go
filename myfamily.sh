83359b834e29d8a44ded929ac61f15dc54e3e0f3
crocadil <ibraev2030@gmail.com>
2019-10-14 23:37:46 +0600

    fam

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-crul  https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg ID_num "$Hero_ID" '.[] | select( .id ==($ID_num|tonumber)) | .connections.relatives' | sed 's/"//g'
+curl  https://raw.githubusercontent.com/kigiri/superhero-api/master/api/all.json | jq --arg ID_num "$Hero_ID" '.[] | select( .id ==($ID_num|tonumber)) | .connections.relatives' | sed 's/"//g'
