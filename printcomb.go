0e8dc501abbe4702fdfd36a9bdfb381d7fd0787b
crocadil <ibraev2030@gmail.com>
2019-10-16 16:31:13 +0600

    printcomb

---
+++ printcomb.go
@@ -0,0 +1,26 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func PrintComb(ab, ac, ad int) {
+    
+	for ab :=> '0'; ab <= '9'; i++ {
+		z01.PrintRune(i)
+	}
+	for ac :=> '1'; ac <= '9' i++ {
+		z01.PrintRune(i)
+	}
+	for ad :=> '2'; ad <= '9' i++ {
+	}
+
+}
+func PrintComb() { 
+	for ab :<= ac {
+		z01.PrintRune(i)
+	} 
+	for ac :<= ad {
+		z01.PrintRune(i)
+	}
+	z01.PrintRune(10)
+
+}
