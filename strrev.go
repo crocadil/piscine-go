61196731528eebb2d412849a730ba07d93bdf05d
crocadil <ibraev2030@gmail.com>
2019-10-17 23:46:31 +0600

    reverse

--- strrev.go
+++ strrev.go
@@ -2,10 +2,9 @@ package piscine
 
 func StrRev(s string) string {
 
-	runes := []rune(s)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	c := ""
+	for range s {
+		c = s + c
 	}
-	return string(runes)
-
+	return c
 }
