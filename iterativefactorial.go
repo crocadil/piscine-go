8fa70302a8782f0480d56c3845d859d45d022235
crocadil <ibraev2030@gmail.com>
2019-10-21 16:15:56 +0600

    iterative

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -2,14 +2,14 @@ package piscine
 
 func IteractiveFactorial(nb int) int {
 
-    if nb >= 0 && nb <= 20 {
-        number := 1
-        for i := 1; i <= nb; i++ {
-            number = number * i
-        }
-        return (number)
-    } else {
-        return 0
-    }
+	if nb >= 0 && nb <= 20 {
+		number := 1
+		for i := 1; i <= nb; i++ {
+			number = number * i
+		}
+		return (number)
+	} else {
+		return 0
+	}
 
 }
