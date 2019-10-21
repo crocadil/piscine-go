b211f74802c21063d613ce0eaeef3e5347da5f66
crocadil <ibraev2030@gmail.com>
2019-10-22 01:02:24 +0600

    fibonacci

--- fibonacci.go
+++ fibonacci.go
@@ -2,10 +2,7 @@ package piscine
 
 func Fibonacci(index int) int {
 
-	if index <= 1 {
-		return index
-	}
-	if index < 0 {
+	if index <= 0 {
 		return -1
 	}
 	return Fibonacci(index-1) + Fibonacci(index-2)
