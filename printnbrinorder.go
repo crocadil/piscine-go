9d93d24c0e47d7e9c1a4ea6cf54bcda470b21fa9
crocadil <ibraev2030@gmail.com>
2019-10-23 18:44:54 +0600

    adikonbr

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -1,4 +1,3 @@
-
 package piscine
 
 import "github.com/01-edu/z01"
@@ -10,31 +9,31 @@ func PrintNbrInOrder(n int) {
 	z := 0
 
 	if n == 0 {
-		adiko = append	(adiko, rune(n))
+		adiko = append(adiko, rune(n))
 	}
 
 	for n != 0 {
 		a = n % 10
 		n = n / 10
-		adiko = append	(adiko, rune(a))
+		adiko = append(adiko, rune(a))
 	}
 
-	for range	adiko {
+	for range adiko {
 		z++
 	}
 
 	for i := 0; i < z-1; i++ {
 		for j := 0; j < z-1-i; j++ {
-			if	adiko[j] >	adiko[j+1] {
-				k :=	adiko[j]
-				adiko[j] =	adiko[j+1]
+			if adiko[j] > adiko[j+1] {
+				k := adiko[j]
+				adiko[j] = adiko[j+1]
 				adiko[j+1] = k
 			}
 		}
 	}
 
 	for i := 0; i < z; i++ {
-		z01.PrintRune	(adiko[i] + 48)
+		z01.PrintRune(adiko[i] + 48)
 	}
-	
+
 }
